7ee0f8ac2813682d5ad85dec5859201ebe1d267a
levitatingmoon <94081701+levitatingmoon@users.noreply.github.com>
2025-01-25 03:51:58 +0100

    Player gets checkpoint

--- lodzjam25/Assets/Scripts/Player/Player.cs
+++ lodzjam25/Assets/Scripts/Player/Player.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float playerHealth = 100f;
+    private Vector3 latestCheckpoint = new Vector3(0.0f,0.0f,0.0f);
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,52 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckForCheckpoint();
+    }
+
+    void DecreaseHealth(float damage)
+    {
+        playerHealth -= damage;
+        if(playerHealth <= 0.0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        transform.position = latestCheckpoint;
+        Debug.Log(transform.position);
+    }
+
+    void CheckForCheckpoint()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, new Vector2(10.0f, 10.0f), 90.0f, transform.forward, LayerMask.GetMask("Checkpoint"));
+        if(hit)
+        {
+            SetLatestCheckpoint(hit.transform.position);
+        }
+
+    }
+
+    void OnDrawGizmos()
+ {
+      // Draw a yellow sphere at the transform's position
+      //Gizmos.color = Color.yellow;
+      //Gizmos.DrawCube(new Vector2(this.transform.position.x, this.transform.position.y), new Vector3(1,1,1));
+     /*
+      Gizmos.color = Color.blue;
+      Gizmos.DrawSphere(this.transform.position + translateSphereOnHight, 2.0f);
+
+      Gizmos.color = Color.magenta;
+      Gizmos.DrawSphere(this.transform.position + new Vector3(0, -0.85f, 0), 0.1f);
+
+      Gizmos.color = Color.gray;
+      Gizmos.DrawSphere(this.transform.position + new Vector3(translateAttackCircle.x, translateAttackCircle.y, 0), 0.3f);*/
+ }
+
+    void SetLatestCheckpoint(Vector3 position)
+    {
+        latestCheckpoint = position;
     }
 }
