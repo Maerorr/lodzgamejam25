fcbda517b474a274c991903866544ce8ccd19a8b
Piotr Mroczek <80900800+DemoKtr@users.noreply.github.com>
2025-01-26 13:02:39 +0100

    fix

--- lodzjam25/Assets/enemies/EnemyMelee.cs
+++ lodzjam25/Assets/enemies/EnemyMelee.cs
@@ -27,7 +27,7 @@ public class EnemyMelee : MonoBehaviour, EnemyBase
     Vector3 positionInit;
     public float patrolDistance;
     Vector2 patrolRange;
-
+    Vector3 initialPositionDebil;
     //
     public float timeToRotate;
     float currentTimeToRotate;
@@ -157,6 +157,7 @@ public class EnemyMelee : MonoBehaviour, EnemyBase
         currentTimeToRotate = timeToRotate;
         currentLifeTimeToDeath = lifeTimeToDeath;
         this.currentBehaviour = new Stay();
+        initialPositionDebil = transform.position;
     }
 
     // Update is called once per frame
@@ -187,7 +188,9 @@ public class EnemyMelee : MonoBehaviour, EnemyBase
               //  GetComponent<Punche>().SpawnBillboard(transform.position);
             }
         }
-        if (transform.position.y < 0) transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        //if (transform.position.y < 0) transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        transform.position = new Vector3(transform.position.x, initialPositionDebil.y, transform.position.z);
+        
         setCurrentState();
         currentBehaviour.execute(this);
     }
